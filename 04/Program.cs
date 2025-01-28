e3da47d9987ca30785c2c11ae945903ee551d31a
yves848 <yves.godart@gmail.com>
2025-01-28 20:23:19 +0100

    04-02

--- 04/Program.cs
+++ 04/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System.Text.RegularExpressions;
 using System.Runtime.InteropServices;
+using System.Net.NetworkInformation;
 
 string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 List<string> file = args.Length > 0 ? File.ReadAllLines(args[0]).ToList() : File.ReadAllLines($"{home}/git/aoc2016/04/test.txt").ToList();
 var lf = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
-
+List<(string name, int id)> valid = [];
 
 void part1()
 {
@@ -28,9 +29,13 @@ void part1()
       });
     });
     string sCheck = "";
-    var sorted = car.OrderByDescending(c => c.Value).ThenBy(c=> c.Key).ToList();
-    sorted.ForEach(c=> sCheck += c.Key.ToString());
-    if (sCheck.StartsWith(checksum)) ans+=id;
+    var sorted = car.OrderByDescending(c => c.Value).ThenBy(c => c.Key).ToList();
+    sorted.ForEach(c => sCheck += c.Key.ToString());
+    if (sCheck.StartsWith(checksum))
+    {
+      ans += id;
+      valid.Add((String.Join(" ", elements), id));
+    }
   });
   Console.WriteLine($"Part 1 - Answer : {ans}");
 }
@@ -38,6 +43,24 @@ void part1()
 void part2()
 {
   int ans = 0;
+  valid.ForEach(v =>
+  {
+    var (key, id) = v;
+    string decrypt = "";
+    key.ToList().ForEach(c =>
+    {
+      if (c == ' ') { decrypt += c.ToString(); }
+      else {
+        for(int i = 0; i < (id % 26); i++) {
+          c++;
+          if (c > 'z') c = 'a';
+        }
+        decrypt += c.ToString();
+      }
+    });
+    if (decrypt.Contains("objects"))
+    Console.WriteLine($"{decrypt}\t{id}");
+  });
   Console.WriteLine($"Part 2 - Answer : {ans}");
 }
 
--- 04/obj/Debug/net9.0/04.AssemblyInfo.cs
+++ 04/obj/Debug/net9.0/04.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afb61318fca0e25aadffc1e2a9f10c36741d6001")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a556bb5a33612e29dae8766239fef314fab7feec")]
 [assembly: System.Reflection.AssemblyProductAttribute("04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
