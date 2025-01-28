a556bb5a33612e29dae8766239fef314fab7feec
yves848 <yves.godart@gmail.com>
2025-01-28 19:34:44 +0100

    04-1

---
+++ 03/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+List<string> file = args.Length > 0 ? File.ReadAllLines(args[0]).ToList() : File.ReadAllLines($"{home}/git/aoc2016/03/data.txt").ToList();
+var lf = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
+Regex reNum = new(@"(\d+)");
+List<(int a, int b, int c)> triangles = [];
+void part1()
+{
+  int ans = 0;
+  file.ForEach(line =>
+  {
+    var m = reNum.Matches(line);
+    var (a, b, c) = (int.Parse(m[0].Value), int.Parse(m[1].Value), int.Parse(m[2].Value));
+    if (a + b > c &&
+        a + c > b &&
+        b + c > a)
+    {
+      ans++;
+    }
+    triangles.Add((a, b, c));
+  });
+  Console.WriteLine($"Part 1 - Answer : {ans}");
+}
+
+void part2()
+{
+  int ans = 0;
+  List<int> ta = triangles.Select(t =>
+  {
+    return t.a;
+  }).ToList();//.Where(p => p >= 100).ToList();
+  ta.Sort();
+  for (int i = 0; i < ta.Count ; i += 3)
+  {
+    var (a, b, c) = (ta[i], ta[i + 1], ta[i + 2]);
+    // if (a / 100 == b / 100 && a / 100 == c / 100)
+      if (a + b > c &&
+          a + c > b &&
+          b + c > a)
+      {
+        ans++;
+      }
+  }
+  ta.Clear();
+  ta = triangles.Select(t =>
+  {
+    return t.b;
+  }).ToList();//.Where(p => p >= 100).ToList();
+  ta.Sort();
+  for (int i = 0; i < ta.Count; i += 3)
+  {
+    var (a, b, c) = (ta[i], ta[i + 1], ta[i + 2]);
+    // if (a / 100 == b / 100 && a / 100 == c / 100)
+      if (a + b > c &&
+          a + c > b &&
+          b + c > a)
+      {
+        ans++;
+      }
+  }
+  ta.Clear();
+  ta = triangles.Select(t =>
+  {
+    return t.c;
+  }).ToList();//.Where(p => p >= 100).ToList();
+  ta.Sort();
+  for (int i = 0; i < ta.Count; i += 3)
+  {
+    var (a, b, c) = (ta[i], ta[i + 1], ta[i + 2]);
+    // if (a / 100 == b / 100 && a / 100 == c / 100)
+      if (a + b > c &&
+          a + c > b &&
+          b + c > a)
+      {
+        ans++;
+      }
+  }
+  Console.WriteLine($"Part 2 - Answer : {ans}");
+}
+
+part1();
+
+part2();
---
+++ 04/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+List<string> file = args.Length > 0 ? File.ReadAllLines(args[0]).ToList() : File.ReadAllLines($"{home}/git/aoc2016/04/test.txt").ToList();
+var lf = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
+
+
+void part1()
+{
+  int ans = 0;
+  Dictionary<char, int> car = [];
+  List<char> check = [];
+  file.ForEach(line =>
+  {
+    var blocs = line.Split("[");
+    string checksum = blocs[1].Replace(']', ' ').Trim();
+    var elements = blocs[0].Split("-").ToList();
+    int id = int.Parse(elements[elements.Count() - 1]);
+    elements.RemoveAt(elements.Count - 1);
+    car.Clear();
+    elements.ForEach(e =>
+    {
+      e.ToList().ForEach(c =>
+      {
+        if (!car.ContainsKey(c)) { car.Add(c, 0); }
+        car[c] += 1;
+      });
+    });
+    string sCheck = "";
+    var sorted = car.OrderByDescending(c => c.Value).ThenBy(c=> c.Key).ToList();
+    sorted.ForEach(c=> sCheck += c.Key.ToString());
+    if (sCheck.StartsWith(checksum)) ans+=id;
+  });
+  Console.WriteLine($"Part 1 - Answer : {ans}");
+}
+
+void part2()
+{
+  int ans = 0;
+  Console.WriteLine($"Part 2 - Answer : {ans}");
+}
+
+part1();
+
+part2();
