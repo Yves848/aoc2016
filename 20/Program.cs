d1a1338e203e7c35bd2f9d9581dc9e7f1553f4e8
yves848 <yves.godart@gmail.com>
2025-02-11 08:39:40 +0100

    20-2

--- 20/Program.cs
+++ 20/Program.cs
@@ -1,24 +1,96 @@
 ﻿using System.Text.RegularExpressions;
 using System.Runtime.InteropServices;
+using System.Diagnostics.CodeAnalysis;
 
 string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-List<string> file = args.Length > 0 ? File.ReadAllLines(args[0]).ToList() : File.ReadAllLines($"{home}/git/aoc2016/20/test.txt").ToList();
+List<string> file = args.Length > 0 ? File.ReadAllLines(args[0]).ToList() : File.ReadAllLines($"{home}/git/aoc2016/20/data.txt").ToList();
 var lf = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
 
+List<(uint min, uint max)> bornes = [];
+
+Regex re = new(@"\d+");
 
 void part1()
 {
-  int ans = 0;
-  file.ForEach(line => {
-    Console.WriteLine(line);
+  uint ans = 0;
+  file.ForEach(line =>
+  {
+    var m = re.Matches(line);
+    uint b1 = uint.Parse(m[0].Value);
+    uint b2 = uint.Parse(m[1].Value);
+    bornes.Add((b1, b2));
   });
+  bornes.Sort();
+  int i = 0;
+  while (i < bornes.Count - 1)
+  {
+    var (min, max) = bornes[i];
+    if (ans >= min && ans <= max)
+    {
+      i++;
+      ans = max + 1;
+      // if (i == bornes.Count) break;
+      continue;
+    }
+    i++;
+  }
   Console.WriteLine($"Part 1 - Answer : {ans}");
 }
 
 void part2()
 {
-  int ans = 0;
-  Console.WriteLine($"Part 2 - Answer : {ans}");
+  uint ans = 0;
+  // bornes.Clear();
+  // file.ForEach(line =>
+  // {
+  //   var m = re.Matches(line);
+  //   uint b1 = uint.Parse(m[0].Value);
+  //   uint b2 = uint.Parse(m[1].Value);
+  //   bornes.Add((b1, b2));
+  // });
+  // bornes.Sort();
+  bornes.ForEach(b =>
+  {
+    Console.WriteLine($"{b.min}-{b.max}");
+  });
+  int i = 0;
+  uint nb = 0;
+  var (min, max) = bornes[i];
+  while (ans <= uint.MaxValue)
+  {
+    if (ans > max)
+    {
+      i++;
+      ans = max + 1;
+      if (i < bornes.Count)
+      {
+        var (m1, m2) = bornes[i];
+        if (ans < m1)
+        {
+          nb++;
+        }
+        if (m1 >= max || m1 >= min)
+          min =  m1;
+        max = Math.Max(max, m2);
+        continue;
+      }
+      else
+      {
+        nb += uint.MaxValue - max;
+        break;
+      }
+    }
+    else
+    {
+      if (ans < min)
+      {
+        nb++;
+        ans++;
+      }
+      else ans = max+1;
+    }
+  }
+  Console.WriteLine($"Part 2 - Answer : {nb}");
 }
 
 part1();
